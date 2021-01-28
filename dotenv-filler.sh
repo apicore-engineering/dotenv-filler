3a2a25de0621164858b253c4e3c96a3606341fe4
Dániel Zoltán Tolnai <daniel.tolnai@apicore.de>
2021-01-28 11:39:22 +0100

    Use -- with printf

--- dotenv-filler.sh
+++ dotenv-filler.sh
@@ -5,14 +5,14 @@ GLOBAL_ENV=$(env)
 
 # Sanity check file path
 if ! [ -f "${DOTENV_PATH}" ]; then
-    echo "Usage: ${0} FILE [VAR_PREFIX]"
+    printf -- 'Usage: %s FILE [VAR_PREFIX]\n' "${0}"
     exit 1
 fi
 
 # Process variable prefix
 if [ -n "${DOTENV_PREFIX}" ]; then
     DOTENV_PREFIX=$( \
-        printf '%s' "${DOTENV_PREFIX}_" \
+        printf -- '%s' "${DOTENV_PREFIX}_" \
             | sed -e 's/[&*+,./:;@|~-]/_/g' \
             | tr -cd '[a-zA-Z0-9_]' \
             | tr '[:lower:]' '[:upper:]' \
@@ -21,9 +21,9 @@ fi
 
 # Process file
 for DOTENV_KEY in $(sed -n 's/^\([a-zA-Z_][a-zA-Z0-9_]*\)=.*$/\1/p' "${DOTENV_PATH}"); do
-    DOTENV_VALUE=$(printf '%s' "${GLOBAL_ENV}" | sed -n "s/^${DOTENV_PREFIX}${DOTENV_KEY}=\\(.*\\)\$/\\1/p")
+    DOTENV_VALUE=$(printf -- '%s' "${GLOBAL_ENV}" | sed -n "s/^${DOTENV_PREFIX}${DOTENV_KEY}=\\(.*\\)\$/\\1/p")
     if [ -n "${DOTENV_VALUE}" ]; then
-        DOTENV_VALUE_ESC=$(printf '%s' "${DOTENV_VALUE}" | sed 's/[&/\]/\\&/g')
+        DOTENV_VALUE_ESC=$(printf -- '%s' "${DOTENV_VALUE}" | sed 's/[&/\]/\\&/g')
         sed -i "s/^\\(${DOTENV_KEY}=\\).*\$/\\1${DOTENV_VALUE_ESC}/" "${DOTENV_PATH}"
     fi
 done
